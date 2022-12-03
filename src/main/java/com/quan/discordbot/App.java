c0583df9cf5a8de9a36f2e55cee44a8f9c210037
Lê Anh Quân <quanrealvn@gmail.com>
2022-12-03 20:16:52 +0700

    v1.0

---
+++ src/main/java/com/quan/discordbot/App.java
@@ -0,0 +1,34 @@
+package com.quan.discordbot;
+
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+import org.javacord.api.entity.intent.Intent;
+import org.javacord.api.entity.message.MessageFlag;
+import org.javacord.api.interaction.SlashCommandInteraction;
+
+public class App {
+	public static void main(String[] args) {
+		String token = "MTA0ODU2OTc2OTIxOTA4NDM0OA.G7Y50l.27K0bdIJONddk48_jDX9DHx0jFFtuSQz-qZddc";
+		DiscordApi api = new DiscordApiBuilder().setToken(token).addIntents(Intent.MESSAGE_CONTENT).login().join();
+
+		api.addMessageCreateListener(event -> {
+			if (event.getMessageContent().equalsIgnoreCase("!invite")) {
+				event.getChannel().sendMessage("Invite me via: " + api.createBotInvite());
+			}
+
+			if (event.getMessageContent().equalsIgnoreCase("!mm09")) {
+				event.getChannel().sendMessage("mm09 pro");
+			}
+		});
+
+		api.addSlashCommandCreateListener(event -> {
+			SlashCommandInteraction slashCommandInteraction = event.getSlashCommandInteraction();
+			if (slashCommandInteraction.getCommandName().equals("help")) {
+				slashCommandInteraction.createImmediateResponder().setContent("This bot is still under development")
+						.append("For now it only has '!mm09' and '!invite' command").setFlags(MessageFlag.CROSSPOSTED)
+						.respond();
+			}
+		});
+
+	}
+}
